8b79dc8e30474520bd412b08c15d7f0977bb830f
deniskipeles <47624821+deniskipeles@users.noreply.github.com>
2023-04-19 01:17:47 +0300

    Update5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { fromPath } = require('pdf2pic');
+const { fromBuffer } = require('pdf2pic');
 const sharp = require('sharp');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = 3000;
@@ -13,38 +14,41 @@ const pdf2picOptions = {
   height: 1080,
 };
 
-app.get('/convert', (req, res) => {
+app.get('/convert', async (req, res) => {
   const documentUrl = req.query.url;
 
-  const convert = fromPath(documentUrl, pdf2picOptions);
+  try {
+    const response = await fetch(documentUrl);
+    const pdfBuffer = await response.buffer();
+    const convert = fromBuffer(pdfBuffer, pdf2picOptions);
 
-  convert
-    .bulk(-1)
-    .then((result) => {
-      const images = [];
+    convert.bulk(-1)
+      .then((result) => {
+        const images = [];
 
-      for (let i = 0; i < result.length; i++) {
-        sharp(result[i].path)
-          .resize({ width: 800 })
-          .toBuffer()
-          .then((buffer) => {
-            images.push(buffer);
+        for (let i = 0; i < result.length; i++) {
+          sharp(result[i].path)
+            .resize({ width: 800 })
+            .toBuffer()
+            .then((buffer) => {
+              images.push(buffer);
 
-            if (images.length === result.length) {
-              res.setHeader('Content-Type', 'application/json');
-              res.send({ images: images });
-            }
-          })
-          .catch((err) => {
-            console.error(err);
-            res.status(500).send({ error: 'An error occurred while processing the images.' });
-          });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send({ error: 'An error occurred while converting the PDF.' });
-    });
+              if (images.length === result.length) {
+                res.setHeader('Content-Type', 'application/json');
+                res.send({ images: images });
+              }
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(port, () => {
