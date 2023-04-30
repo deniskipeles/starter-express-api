9d4e8809864aa57a8632c7a2bf3d21fd92cdba9e
deniskipeles <47624821+deniskipeles@users.noreply.github.com>
2023-04-30 12:01:23 +0300

    express Js

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM alpine:3.16
 
 ENV NODE_VERSION 20.0.0
 
+RUN apk update
+RUN apk add --no-cache imagemagick bash pngcrush optipng=0.7.7-r0
+# CMD ["/bin/bash"]
+
 RUN addgroup -g 1000 node \
     && adduser -u 1000 -G node -s /bin/sh -D node \
     && apk add --no-cache \
--- server.js
+++ server.js
@@ -1,15 +1,62 @@
-'use strict';
-
 const express = require('express');
+const request = require('request');
+const { fromBuffer } = require('pdf2pic');
+const sharp = require('sharp');
 
-// Constants
+const app = express();
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
+const pdf2picOptions = {
+  density: 300,
+  savePath: './images',
+  format: 'png',
+  width: 1920,
+  height: 1080,
+};
+
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/convert', (req, res) => {
+  const documentUrl = req.query.url;
+
+  request.get({ url: documentUrl, encoding: null }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      res.status(400).send('Error fetching PDF');
+      return;
+    }
+
+    const convert = fromBuffer(body, pdf2picOptions);
+
+    convert.bulk(-1)
+      .then((result) => {
+        const images = [];
+
+        for (let i = 0; i < result.length; i++) {
+          sharp(result[i].path)
+            .resize({ width: 800 })
+            .toBuffer()
+            .then((buffer) => {
+              images.push(buffer);
+
+              if (images.length === result.length) {
+                res.setHeader('Content-Type', 'application/json');
+                res.send({ images: images });
+              }
+            })
+            .catch((err) => {
+              console.error(err);
+              res.status(500).send('Error processing PDF');
+            });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error converting PDF to images');
+      });
+  });
 });
 
 app.listen(PORT, HOST, () => {
